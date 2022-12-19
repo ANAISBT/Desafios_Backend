28a99a6417d8dcde1685e835a6ec3e7f25a22e85
ANAIS <anais.bustamante@unmsm.edu.pe>
2022-12-19 17:33:48 -0500

    Desafio5

---
+++ desafio5/server.js
@@ -0,0 +1,34 @@
+const express= require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+const port= 8080;
+
+const server= app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+}
+);
+
+server.on("error", error => console.log(`Error en servidor ${error}`));
+
+//-------------------------------------------
+
+const productos= [];
+
+app.get("/productos", (req, res) => {
+    res.render("pages/Formulario",{productos: productos,title: "Formulario de productos"});
+});
+
+app.post("/productos", (req, res) => {
+    const producto= req.body;
+    productos.push(producto);
+    res.redirect("/productos");
+});   
